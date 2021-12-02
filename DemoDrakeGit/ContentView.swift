b315ee84565f060f276be938ec3f8834ca737bb9
Mickaël MAS <appiwedia@gmail.com>
2021-12-02 14:35:18 +0100

    Initial Commit

---
+++ DemoDrakeGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoDrakeGit
+//
+//  Created by Mickael Mas on 02/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoDrakeGit/DemoDrakeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDrakeGitApp.swift
+//  DemoDrakeGit
+//
+//  Created by Mickael Mas on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoDrakeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
