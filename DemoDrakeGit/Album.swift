707b30ed6f19c9bb2d2e0833c3006543d020bbf3
Mickaël MAS <appiwedia@gmail.com>
2021-12-02 14:42:58 +0100

    Ajout Image Drake + Structure Album

---
+++ DemoDrakeGit/Album.swift
@@ -0,0 +1,15 @@
+//
+//  Album.swift
+//  DemoDrakeGit
+//
+//  Created by Mickael Mas on 02/12/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct Album {
+    let image: Image
+    let title: String
+    let artist: String
+}
